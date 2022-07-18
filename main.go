71c201d03642476f3808dc768d12ebb78b0edb42
sammeboy635 <Samueltoddwaite@gmail.com>
2022-07-18 07:55:39 -0600

    templates and ranges working

--- main.go
+++ main.go
@@ -1,17 +1,47 @@
 package main
 
 import (
-	"log"
+	"html/template"
 	"net/http"
 )
 
-func main() {
-	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fs)
+type ToDo struct {
+	Name     string
+	College  string
+	Keywords []string
+}
 
-	log.Print("Listening on :3000...")
-	err := http.ListenAndServe(":3000", nil)
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	todos := ToDo{Name: "test", College: "testeset", Keywords: []string{"here", "here2"}}
+
+	t, _ := template.ParseFiles("static/index.html",
+		"static/html/nav.html",
+		"static/html/footer.html",
+		"static/html/card.html")
+	err := t.Execute(w, todos)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
+}
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	todos := ToDo{Name: "test", College: "testeset"}
+
+	t, _ := template.ParseFiles("static/home.html",
+		"static/html/nav.html",
+		"static/html/footer.html",
+		"static/html/card.html")
+	err := t.Execute(w, todos)
+	if err != nil {
+		panic(err)
+	}
+}
+func main() {
+
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/home", homeHandler)
+
+	http.ListenAndServe(":3000", nil)
 }
